2e2b41b215842cd341f644d289104abfe687cf47
Gowtham M <108858082+gowthamtn37@users.noreply.github.com>
2022-07-25 20:44:42 +0530

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,26 @@
+//1. print odd number in arrow function
+let int1 = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+let odds = int1.filter((n) => n%2 !== 0);
+
+
+console.log(odds)
+
+//2. Convert all the string to title caps in string array
+
+
+
+
+//3. sum of all numbers in array
+
+let int3 = [1,2,3,4,5,6,7,8,9,10,11,12];
+let sum = int3.reduce((acc,e)=>acc+e);
+console.log([sum]);
+
+//4. return all the prime number in an array
+
+
+
+
+
+//5. return all the palindrones in an array
